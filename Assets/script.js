d61a12d9616c144c49480f3c0c019d6074916ac8
Shawn Chang <shawnchang@ShawnChang-m62.local>
2022-07-10 15:54:14 -0700

    challenge 4 code quiz

---
+++ Assets/script.js
@@ -0,0 +1,8 @@
+// Coding Quiz Challenge:
+// 1. create a timed quiz on Javascript fundamentals
+// 2. click the Start button, timer counts down
+// 3. wrong answers subtract time quickly
+// 4. game is over when time is over OR all questions answered correctly
+// 5. player can save their high score and initial
+
+var startBtn = document.getElementById("startBtn");
